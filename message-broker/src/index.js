a60b78b5e093620a221b5d18f9d1f616e8bdc179
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-24 22:46:56 +0200

    Message Broker

---
+++ message-broker/src/index.js
@@ -0,0 +1,28 @@
+const { tmiClient } = require("./tmiClient");
+const { publisher } = require("./publisher");
+const { subscriber } = require("./subscriber");
+
+subscriber.subscribe("say");
+subscriber.on("message", (channel, message) => {
+    switch (channel) {
+        case "say":
+            const { target, msg } = JSON.parse(message);
+            if (!target || !msg) {
+                return;
+            }
+
+            tmiClient.say(target, msg);
+            break;
+    }
+});
+
+tmiClient.on("message", (channel, context, msg, self) => {
+    publisher.publish(
+        "message",
+        JSON.stringify({ channel, context, msg, self })
+    );
+});
+
+tmiClient.on("connected", (addr, port) => {
+    console.log(`* Connected to ${addr}:${port}`);
+});
