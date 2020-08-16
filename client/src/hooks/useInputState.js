bc26db88efc98caa15583e430178a93067eaa98a
angens0 <kamil.patryk.sygut@gmail.com>
2020-08-16 02:15:21 +0200

    Client: useInputState hook

---
+++ client/src/hooks/useInputState.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const useInputState = initialValue => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChange = event => setValue(event.target.value);
+
+    const reset = () => setValue("");
+
+    return [value, handleChange, reset];
+};
