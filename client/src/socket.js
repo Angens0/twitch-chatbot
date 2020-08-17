1cfdfc58a5152df826e89df2c2f2005316cc5d77
unknown <kamil.patryk.sygut@gmail.com>
2020-08-17 02:55:12 +0200

    Prod setup test

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000", { path: "/api/sounds/socket.io" });
+const socket = io("", { path: "/api/sounds/socket.io" });
 socket.on("connect", () => {
     console.log("connected to socket server");
 });
