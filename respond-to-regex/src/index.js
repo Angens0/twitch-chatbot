a99110d00444d184df807cdc61b97957f7433d56
Angens0 <kamil.patryk.sygut@gmail.com>
2021-02-15 23:17:20 +0100

    ResidentSleeper detector temp

--- respond-to-regex/src/index.js
+++ respond-to-regex/src/index.js
@@ -30,6 +30,43 @@ const start = async () => {
                         JSON.stringify({ target: channel, msg: response })
                     );
                 }
+
+                const username = context.username;
+                const sleeperCount = (msg.match(/ResidentSleeper/gi) || [])
+                    .length;
+
+                const getRandomIntInclusive = (min, max) => {
+                    min = Math.ceil(min);
+                    max = Math.floor(max);
+                    return Math.floor(Math.random() * (max - min + 1)) + min;
+                };
+
+                if (sleeperCount > 0) {
+                    let probability = Math.min(
+                        Math.log10(sleeperCount * 2) * 0.5,
+                        0.5
+                    );
+                    probability = Math.round(probability * 100) / 100;
+                    publisher.publish(
+                        "say",
+                        JSON.stringify({
+                            target: channel,
+                            msg: `ResidentSleeper wykryty. Szansa na t/o wynosi: ${
+                                probability * 100
+                            }%`,
+                        })
+                    );
+                    if (getRandomIntInclusive(1, 100) <= probability * 100) {
+                        publisher.publish(
+                            "say",
+                            JSON.stringify({
+                                target: channel,
+                                msg: `/timeout ${username}`,
+                            })
+                        );
+                    }
+                }
+
                 break;
         }
     });
